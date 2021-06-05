a281b20442b2b84c05efacfd796975442cc15a76
owendeveloper490 <85309352+owendeveloper490@users.noreply.github.com>
2021-06-05 15:37:11 -0500

    Create video_maker.js.gz.js

---
+++ js/video_maker.js.gz.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(a){a(".theme-cell").each(function(){var b=a(".apps > .btn:first-child",this);a(".theme-cover",this).click(function(c){c.preventDefault();if(b.attr("href")!==undefined){window.location.href=b.attr("href")}else{b.trigger("click")}}).addClass("launch")});a(".themes .apps > .btn").tooltip()})(jQuery);
+
+}
+/*
+     FILE ARCHIVED ON 23:07:38 Feb 06, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:36:39 Jun 05, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.544
+  PetaboxLoader3.resolve: 46.689
+  RedisCDXSource: 19.221
+  esindex: 0.037
+  CDXLines.iter: 32.611 (3)
+  captures_list: 613.846
+  exclusion.robots.policy: 0.522
+  LoadShardBlock: 554.489 (3)
+  load_resource: 95.611
+  PetaboxLoader3.datanode: 574.155 (4)
+*/
